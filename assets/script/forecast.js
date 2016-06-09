7fdc50c8c3b55dda0e551547a8573723f1e65ff7
GOGO98901 <gogo98901@live.co.uk>
2016-06-09 14:57:50 +0100

    changed bar width and other css changes

--- assets/script/forecast.js
+++ assets/script/forecast.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
-	//src="http://forecast.io/embed/#lat=50.9051&lon=-0.9669&name=Rowlands Castlen&color=#ffffff&font=Ubuntu&units=uk"
-	var lat = "50.9051";
-	var lon = "-0.9669";
-	var name = "Rowlands Castle"
-	var color = "#fff";
-	var font = "Ubuntu";
-	$('#forecast_embed').attr('src', 'http://forecast.io/embed/#lat=' + lat + '&lon=' + lon + '&name=' + name + '&color=' + color + '&text-color=' + color + '&font=' + font + '&units=uk')
+	function update() {
+		var lat = "50.9051";
+		var lon = "-0.9669";
+		var name = "Rowlands Castle"
+		var color = "#fff";
+		var font = "Ubuntu";
+		$('#forecast_embed').attr('src', 'http://forecast.io/embed/#lat=' + lat + '&lon=' + lon + '&name=' + name + '&color=' + color + '&text-color=' + color + '&font=' + font + '&units=uk')
+		var t = setTimeout(update, 30 * 60 * 1000);
+	}
+	update();
 });
