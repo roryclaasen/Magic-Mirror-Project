16e40fe5917b236eeeb0097c2a0fa4f532a86372
GOGO98901 <gogo98901@live.co.uk>
2016-06-10 10:56:40 +0100

    using new and better weather!

--- assets/script/forecast.js
+++ assets/script/forecast.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
-	function update() {
-		var lat = "50.9051";
-		var lon = "-0.9669";
-		var name = "Rowlands Castle"
-		var color = "#fff";
-		var font = "Ubuntu";
-		$('#forecast_embed').attr('src', 'http://forecast.io/embed/#lat=' + lat + '&lon=' + lon + '&name=' + name + '&color=' + color + '&text-color=' + color + '&font=' + font + '&units=uk')
-		var t = setTimeout(update, 30 * 60 * 1000);
-	}
-	update();
+	getWeather();
+	setInterval(getWeather, 10 * 60 * 1000);
 });
+
+function getWeather() {
+	$.simpleWeather({
+		location: 'Rowlands Castle',
+		unit: 'c',
+		success: function(weather) {
+			html = '<h2><i class="wtr icon-' + weather.code + '"></i> ' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
+			html += '<ul><li>' + weather.city + ', ' + weather.region + '</li>';
+			html += '<li class="currently">' + weather.currently + '</li>';
+			html += '<li>' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed + '</li></ul>';
+			html += '<div class="forecast">';
+			for(var i = 0; i < /*weather.forecast.length*/ 6; i++) {
+				html += '<div><p><span class="day">' + weather.forecast[i].day + '</span><i class="wtr icon-' + weather.forecast[i].code + '"></i><span class="temp">' + weather.forecast[i].high + '</span></p></div>';
+			}
+			html += '</div>';
+			$("#weather").html(html);
+		},
+		error: function(error) {
+			$("#weather").html('<p>' + error + '</p>');
+		}
+	});
+}
