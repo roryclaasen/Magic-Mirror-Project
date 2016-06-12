ef094adc3ca8514ca02191930dad00da035f3cd7
GOGO98901 <gogo98901@live.co.uk>
2016-06-12 21:25:26 +0100

    updated startserver script (Batch)

--- startserver.bat
+++ startserver.bat
@@ -4,10 +4,15 @@ cd site
 :start
 cls
 python -m http.server 8000
-pause
+:exit
 set INPUT=
 set /P INPUT="Do you want to restart? (yes/no) "
 if "%INPUT%"=="y" goto start
 if "%INPUT%"=="Y" goto start
 if "%INPUT%"=="yes" goto start
 if "%INPUT%"=="Yes" goto start
+if "%INPUT%"=="n" exit
+if "%INPUT%"=="N" exit
+if "%INPUT%"=="no" exit
+if "%INPUT%"=="No" exit
+goto exit
