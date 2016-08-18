598d4bb56e9a1e2bac829cbee1f6ebb4694d9367
GOGO98901 <gogo98901@live.co.uk>
2016-08-18 15:29:35 +0100

    trying to fix crash

--- autostart.sh
+++ autostart.sh
@@ -9,7 +9,8 @@ server() {
 display() {
 	sleep 5
 	sudo -u pi epiphany-browser -a --profile ~/.config http://localhost:8000 &
-	xte 'sleep 15' 'key F11' -x:0
+	sleep 15s;
+	xte "key F11" -x:0
 }
 init
 server &
