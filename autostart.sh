5d87306f7adc3a536046239fa0221e02a296f141
GOGO98901 <gogo98901@live.co.uk>
2016-08-18 15:19:05 +0100

    rewrote autostart

--- autostart.sh
+++ autostart.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-
-cd /home/pi/git/mirror/
-git pull
-lxterminal -e ./startserver.sh
-
-sleep 5
-sudo -u pi epiphany-browser -a --profile ~/.config http://localhost:8000 &
-xte 'sleep 15' 'key F11' -x:0
+init() {
+	cd /home/pi/git/mirror/
+	git pull
+}
+server() {
+	lxterminal -e ./startserver.sh
+}
+display() {
+	sleep 5
+	sudo -u pi epiphany-browser -a --profile ~/.config http://localhost:8000 &
+	xte 'sleep 15' 'key F11' -x:0
+}
+init
+server &
+display
 exit 0
