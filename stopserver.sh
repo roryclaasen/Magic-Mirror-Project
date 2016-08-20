1b9cecb67dc2ea3769fc2590f2e52699cbc60764
Rory Claasen <GOGO98901@users.noreply.github.com>
2016-08-20 15:11:44 +0100

    Update stopserver.sh

--- stopserver.sh
+++ stopserver.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo -----------------------------
+echo ------ Stopping Server ------ 
+echo -----------------------------
 killall epiphany # Shuts down the browser
 killall lxterminal # Shuts down ALL terminal windows
