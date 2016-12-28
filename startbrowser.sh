cf9ed6d1e255420144f3c46f2f5ac13298b7e4d7
Rory Claasen <GOGO98901@users.noreply.github.com>
2016-12-28 23:53:49 +0000

    Update startbrowser.sh

--- startbrowser.sh
+++ startbrowser.sh
@@ -2,7 +2,7 @@
 echo -----------------------------
 echo ----- Browser  Starting -----
 echo -----------------------------
-sudo -u pi epiphany-browser -a --profile ~/.config http://0.0.0.0:8000 --display=:0 &
+sudo -u /usr/bin/chromium --kiosk --ignore-certificate-errors --disable-restore-session-state "http://0.0.0.0:8000" &
 sleep 30s;
 xte "key F11" -x:0
 xte "key F5" -x:0 # Just in case
