6b1a53a35531824ab2cca741fd1e955a54dc20d7
Rory <rory@claasen.biz>
2017-05-12 19:16:11 +0100

    removed old scripts

--- scripts/readme.md
+++
@@ -1,1 +0,0 @@
-# This is a temporary folder while I fix the project
--- scripts/updateserver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/pi/git/mirror/
-echo -----------------------------
-echo ---     Attempting to     ---
-echo --- update display files  ---
-echo -----------------------------
-echo Attempting to update display files
-git status
-git pull
