9eaac55b3394bbf5711b5ea253e0a8940b8ee280
Rory <rory@claasen.biz>
2017-05-13 18:13:26 +0100

    some quick changes before a reboot test

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ else
     echo "Git repository MAY not be up to date"
 fi
 
-yes | sudo apt-get x11-xserver-utils unclutter
+yes | sudo apt-get install x11-xserver-utils unclutter
 
 echo "@lxpanel --profile LXDE-pi
 @pcmanfm --desktop --profile LXDE-pi
@@ -21,15 +21,15 @@ echo "@lxpanel --profile LXDE-pi
 @xset -dpms
 @xset s noblank" > ~/.config/lxsession/LXDE-pi/autostart
 
-echo "lxterminal --command \"$PWD/start.sh\"" >> ~/.config/lxsession/LXDE-pi/autostart
+echo "lxterminal -e \"$PWD/start.sh\"" >> ~/.config/lxsession/LXDE-pi/autostart
 
 echo "Making sure that Ruby and Jekyll are installed on this system"
 yes | sudo apt-get install ruby-full
 sudo gem install jekyll
 
 echo "A reboot is required for this display to work"
-read -r -p "Do you want to reboot now? [y/N] " response
-response=${response,,}
-if [[ "$response" =~ ^(yes|y)$ ]]
+read -p "Do you want to reboot now? [y/N]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
     sudo reboot
 fi
